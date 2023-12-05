ce76c7167cadb42e8cc11e3072e3683cc0a11414
Kajojan <kojoty1999@gmail.com>
2023-12-05 13:11:21 +0100

    fix  problem with kids in csv_loader

--- main/db/file_type/csv_loader.py
+++ main/db/file_type/csv_loader.py
@@ -10,6 +10,19 @@ class CSV_Loader(Loder):
             for row in reader:
                 data_row = {}
                 for index, value in enumerate(row):
-                    data_row[headers[index]] = value
+                    if headers[index] == "children" and len(value) > 0:
+                        children_list = []
+                        children_data = value.split(",")
+                        for child_info in children_data:
+                            child_name, child_age = child_info.strip().split(" ")
+                            child_age = child_age.replace("(", "")
+                            child_age = child_age.replace(")", "")
+                            children_list.append(
+                                {"name": child_name, "age": int(child_age)}
+                            )
+                        data_row[headers[index]] = children_list
+                    else:
+                        data_row[headers[index]] = value
                 self.data.append(data_row)
+
         return self.data
