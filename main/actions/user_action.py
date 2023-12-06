9867d8bd3d1a9b7f041913135f5ab402dd061245
Kajojan <kojoty1999@gmail.com>
2023-12-06 23:44:15 +0100

    user_action class

---
+++ main/actions/user_action.py
@@ -0,0 +1,53 @@
+import lit
+import pandas as pd
+import json
+import numpy as np
+
+
+class User:
+    def __init__(self, db, user):
+        self.dataBase = db
+        self.users = self.dataBase.get_data()
+        self.user = user[0]
+
+    def print_children(self):
+        children = self.user["children"]
+        children_sorted = sorted(children, key=lambda x: x["name"])
+        return children_sorted
+
+    def find_similar_children_by_age(self):
+        data = self.users
+        result = []
+        for user in data:
+            if user != self.user:
+                children = list(filter(lambda y: y["age"] == 6, user["children"]))
+                if len(children) > 0:
+                    helper = {
+                        "firstname": user["firstname"],
+                        "telephone_number": user["telephone_number"],
+                        "children": user["children"],
+                    }
+                    result.append(helper)
+
+        return result
+
+    def find_similar_childre_by_age_SQL(self):
+        children = self.user["children"]
+        id = self.user["id"]
+        data = []
+        for kids in children:
+            other = self.dataBase.get_other_children(kids["age"], id)
+            result_dict = [
+                {
+                    "firstname": firstname,
+                    "telephone_number": telephone_number,
+                    "children": sorted(json.loads(children), key=lambda x: x["name"]),
+                }
+                for firstname, telephone_number, children in other
+            ]
+            data.append(result_dict)
+        list = [obj for sublist in data for obj in sublist]
+        df = pd.DataFrame(list)
+        df_no_duplicates = df.drop_duplicates(subset="telephone_number")
+        unique_data = df_no_duplicates.to_dict(orient="records")
+        return unique_data
